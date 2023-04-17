5e4e5f121bc60820de45bc15d7b33bc55c6ab281
Nensi Skenderi <nensiskenderi20@gmail.com>
2023-04-17 20:32:09 +0200

    Initial commit

---
+++ src/main/java/com/learning/springbootdockerk8s/SpringBootWithDockerAndK8sApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springbootdockerk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithDockerAndK8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithDockerAndK8sApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/springbootdockerk8s/SpringBootWithDockerAndK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springbootdockerk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithDockerAndK8sApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
